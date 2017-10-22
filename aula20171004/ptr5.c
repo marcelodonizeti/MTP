38af76dc77134e5dafb56676b52554432f450376
Marcelo Donizeti <marcelo15899@hotmail.com>
2017-10-22 20:42:00 -0200

    fim de aula

---
+++ aula20171004/ptr5.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+#define n 1000
+int main()
+{
+    int vet[n], i,j=0,k=0;
+    unsigned char *bytes;
+    int escolha, cont[n]={0};
+    printf("Escolha um numero de 0 a 255:\n");
+    scanf("%i", &escolha);
+    srand(time(0));
+    for(i=0,bytes=(unsigned char*)vet; i<n;i++)    {
+
+        vet[i]=rand()%(RAND_MAX);
+    }
+
+    for(j=0; j<1000;j++,bytes+=4){
+
+        if(*bytes==escolha){
+
+            printf("0x%p : %d\n",bytes,*bytes);
+            cont[k]=bytes;
+            k++;
+
+
+        }
+
+
+    }
+
+printf("Esses %d bytes sao iguais a %d e estao localizados entre o endereco %p ate o endereco %p na memoria",k,escolha,cont[0],cont[k-1]);
+
+}
