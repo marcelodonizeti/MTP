d12dc7bc5985184cf5b4a495142563acfcbcb378
Marcelo Donizeti <marcelo15899@hotmail.com>
2017-10-23 12:22:30 -0200

    fim de aula

--- aula20171011/casr2.c
+++ aula20171011/casr2.c
@@ -3,7 +3,19 @@
 #include <string.h>
 #include <locale.h>
 #include <time.h>
-
+void arteabstrata(int linhas, int colunas, char caracteres[])
+{
+    int i=0, j=0, a=0;
+    for(i=0;i<linhas;i++)
+    {
+        printf("\n");
+        for(j=0;j<colunas;j++)
+        {
+            a=rand()%strlen(caracteres);
+            printf("%c", caracteres[a]);
+        }
+    }
+}
 int main()
 {
     srand(time(0));
@@ -12,15 +24,7 @@ int main()
     char caracteres[] = ": $ # $ : 4 b . ' :. : $ # $: 4b. ':.";
     printf("Digíte o número de linhas e colunas (linhas,colunas): ");
     scanf("%d,%d", &linhas, &colunas); getchar();
-    for(i=0;i<linhas;i++)
-    {
-        printf("\n");
-        for(j=0;j<colunas;j++)
-        {
-            a=rand()%38;
-            printf("%c", caracteres[a]);
-        }
-    }
+    arteabstrata(linhas,colunas, caracteres);
     printf("\n\n\n\n\n");
     return EXIT_SUCCESS;
 }
