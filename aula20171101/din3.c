9e9459abbafd5495f258e86b06dee7c1bceb9c22
Marcelo Donizeti <marcelo15899@hotmail.com>
2017-11-24 01:04:40 -0200

    fim de aula

--- aula20171101/din3.c
+++ aula20171101/din3.c
@@ -1,8 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define     N 256
+#include<stdlib.h>
+#include<stdio.h>
+#include <ctype.h>
 int main()
 {
-   char *ptr[N];
-   *ptr= realloc
+    char * str;
+    int i=0, j=0;
+    str=(char*)calloc(1,sizeof(int));
+    printf("Entre com uma frase:\n");
+    while(!0)
+    {
+        scanf("%c", str+i);
+        str= (char *)realloc(str, sizeof(char)*1);
+        if((str+i)==NULL)
+        {
+            printf("\n\nSem memoria suficiente\n\n");
+            return EXIT_SUCCESS;
+        }
+        if(*(str+i)== 10)
+            break;
+        i++;
+    }
+    *(str+i)='\0';
+    while(i>=0)
+    {
+        *(str+i)= toupper(*(str+i));
+        i--;
+    }
+    printf("Frase salva na memoria (maiuscula): %s\n\n",str);
+    free(str);
+    return EXIT_SUCCESS;
 }
